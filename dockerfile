e7c196a5e7cf7b77e53ba6dd0b1bb27b3ec2bd74
cowanAI <fern@cowanagency.com>
2023-08-14 18:08:41 +0200

    cowan vision final

--- dockerfile
+++ dockerfile
@@ -1,5 +1,14 @@
 FROM python:3.11-buster
 
+# Install OpenGL library
+RUN apt-get update && apt-get install -y \
+    libgl1-mesa-glx \
+    libglib2.0-0 \
+    libsm6 \
+    libxext6 \
+    libxrender-dev \
+    ffmpeg \
+    && rm -rf /var/lib/apt/lists/*
 EXPOSE 80
 
 # Install pip requirements
@@ -8,7 +17,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 WORKDIR /app
 COPY . /app
-
 
 # During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -3,13 +3,20 @@ import torch
 from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
 from diffusers.utils import export_to_video
 
+
+
+if torch.cuda.is_available():
+    print("CUDA is available!")
+else:
+    print("CUDA is not available.")
+
 pipe = DiffusionPipeline.from_pretrained("cerspense/zeroscope_v2_576w", torch_dtype=torch.float16)
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe.enable_model_cpu_offload()
 
 def infer(prompt):
     negative_prompt = "text, watermark, blurry, nsfw"
-    video_frames = pipe(prompt, negative_prompt=negative_prompt, num_inference_steps=40, height=480, width=720, num_frames=24).frames
+    video_frames = pipe(prompt, negative_prompt=negative_prompt, num_inference_steps=40, height=320, width=576, num_frames=24).frames
     video_path = export_to_video(video_frames)
     print(video_path)
     return video_path
@@ -35,4 +42,5 @@ ui= gr.Interface(
     analytics_enabled=False
 )
 
+ui.queue()
 ui.launch(server_name="0.0.0.0", server_port=80)
